6a87338e5c8b1f8bd10a0e4bfd2d222be4ceb31e
hafiz <hafiz@gmail.com>
2023-08-28 15:22:29 +0400

    Header UI was prepared

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-
+import Header from "./components/Header"
+import { BrowserRouter as Router, Routes,Route, Navigate } from 'react-router-dom';
+import history from "./components/history";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Router history={history}>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/home"></Navigate>}></Route>
+        <Route exact path='/home' element={<Header/>}></Route>
+      </Routes>
+    </Router>
+
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Navigate to="home" replace={true}></Navigate>}></Route>
+    //     <Route path="home" element={<div><Header></Header><Outlet></Outlet></div>}>
+    //       <Route path="" element={<div className="absolute left-1/2 top-1/2">Hello</div>}></Route>
+    //     </Route>
+    //     <Route path="*" element={<Navigate to="home" replace={false}></Navigate>}></Route>
+    //   </Routes>
+    // </BrowserRouter>
   )
 }
 
